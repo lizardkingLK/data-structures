52995547996de4ead0b1b251765ccf8f67e8e723
lizardkinglk <chansanfdo@gmail.com>
2026-03-15 20:05:26 +0530

    Updated data structures
    - Resolved conflicts

--- src/dataStructures.Program/Program.cs
+++ src/dataStructures.Program/Program.cs
@@ -1,18 +1,9 @@
-﻿using dataStructures.Core.NonLinear.Tries;
-
-namespace dataStructures.Program;
+﻿namespace dataStructures.Program;
 
 class Program
 {
     static void Main()
     {
-        SuffixTree st = new();
-        Trie trie = new();
-        trie.Insert("fo");
-        trie.Insert("fo sho");
-        trie.Insert("fo real");
-        trie.Insert("foobar");
-        trie.Insert("fus-ro-dah");
-        
+
     }
 }
